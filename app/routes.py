7ad463b8f02a05e227e253a1fe5abc5cec83aa70
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-04-30 11:04:34 -0700

    adds setup code

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,8 @@
 from flask import Blueprint
 
+solar_system_bp = Blueprint("solar_system", __name__)
+
+@solar_system_bp.route("/solar-system", methods=["GET"])
+def endpoint_name():
+    ss_response_body = "Welcome to the Solar System!"
+    return ss_response_body
