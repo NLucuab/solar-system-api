59f0ffcb005d9ac9287dce98f824370004c65751
Summer <summmick@gmail.com>
2021-04-30 12:59:41 -0700

    updates roots

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,11 @@ from flask import request
 
 
 solar_system_bp = Blueprint("solar_system", __name__)
+planets_bp = Blueprint("", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["POST"])
+def planet():
+    request_body = request.get_json()
 
 @solar_system_bp.route("/solar-system", methods=["GET"])
 def endpoint_name():
