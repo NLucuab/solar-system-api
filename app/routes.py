ded4dce990c47c913179094a4049e5c82c8ec567
Summer <summmick@gmail.com>
2021-04-30 13:11:59 -0700

    adds post request

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from app import db 
 from flask import Blueprint
 from flask import request
 
@@ -8,6 +9,16 @@ planets_bp = Blueprint("", __name__, url_prefix="/planets")
 @planets_bp.route("", methods=["POST"])
 def planet():
     request_body = request.get_json()
+
+    new_planet = Planet(name = request_body["name"], 
+    description = request_body["description"], 
+    size = request_body["size"])
+
+    db.session.add(new_planet)
+    db.commit()
+
+    return (f"planet #{new_planet.name} has been created.", 201)
+
 
 @solar_system_bp.route("/solar-system", methods=["GET"])
 def endpoint_name():
