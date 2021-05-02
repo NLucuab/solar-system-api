e365c842477052b3ee7b359a3fed3de0ca80023d
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-05-02 12:20:56 -0700

    adds strict_slashes notation

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app.models.planet import Planet
 solar_system_bp = Blueprint("solar_system", __name__)
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@planets_bp.route("", methods=["POST"])
+@planets_bp.route("", methods=["POST"], strict_slashes = False)
 def planet():
     request_body = request.get_json()
 
@@ -16,10 +16,10 @@ def planet():
     db.session.add(new_planet)
     db.session.commit()
 
-    return (f"planet #{new_planet.name} has been created.", 201)
+    return (f"planet {new_planet.name} has been created.", 201)
 
 
-@solar_system_bp.route("/solar-system", methods=["GET"])
-def endpoint_name():
-    ss_response_body = "Welcome to the Solar System!"
-    return ss_response_body
+# @solar_system_bp.route("/solar-system", methods=["GET"])
+# def endpoint_name():
+#     ss_response_body = "Welcome to the Solar System!"
+#     return ss_response_body
