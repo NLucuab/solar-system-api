15c5d3c01ca62cbd248566ac927a01fde2dbe98c
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-05-04 15:05:29 -0700

    more updates

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,7 @@ load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     if not test_config:
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
