dac35119ac14f71acbb1883301329c6ae3b8b059
Nyckolle Lucuab <66242273+NLucuab@users.noreply.github.com>
2021-05-04 00:35:23 -0700

    adds configuration for test environment

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,11 @@ migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    if not test_config:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_test'
 
     db.init_app(app)
     migrate.init_app(app, db)
