626553b4353174566ef2b37776a7244c77fb5c0b
Summer <summmick@gmail.com>
2021-05-04 14:34:38 -0700

    Add new test to get planet by its name

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -13,13 +13,26 @@ def test_create_one_planet(client):
     assert response_body['success'] == True
 
 def test_get_one_planet(client, two_saved_planets):
-    #Act
+    # Act
     response = client.get("/planets/1")
     response_body = response.get_json()
 
-    #Assert
+    # Assert
     assert response.status_code == 200
-    assert response_body["id"] == 1
-    assert response_body["name"] == "Swirly Planet"
-    assert response_body["description"] == "a swirl planet"
-    assert response_body["size"] == "Small"
+    assert_planet(response_body)
+
+def test_get_planet_by_name(client, two_saved_planets):
+    # Act
+    response = client.get("/planets", query_string={"name": "Swirly Planet"})
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert_planet(response_body[0])
+
+
+def assert_planet(actual):
+    assert actual["id"] == 1
+    assert actual["name"] == "Swirly Planet"
+    assert actual["description"] == "a swirl planet"
+    assert actual["size"] == "Small"
